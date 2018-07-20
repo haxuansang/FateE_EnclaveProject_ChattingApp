7a465e8a268439dad7ce6fbc4657501ba34ee090
Loc V. TRAN <vanlocbk1996@gmail.com>
2018-07-20 10:18:52 +0700

    change_survey_status_constant

--- app/src/main/java/application/android/com/fatee/utils/SurveyConstant.java
+++ app/src/main/java/application/android/com/fatee/utils/SurveyConstant.java
@@ -3,8 +3,8 @@ package application.android.com.fatee.utils;
 public final class SurveyConstant {
     public static String TAG_DELIM = "%";
     public static final String USER_SURVEY_STATUS_KEY = "surveyStatus";
-    public static final String USER_NO_FINISH_SURVEY_STATUS = "F";
-    public static final String USER_FINISHED_SURVEY_STATUS = "T";
+    public static final String USER_NO_FINISH_SURVEY_STATUS = "FALSE";
+    public static final String USER_FINISHED_SURVEY_STATUS = "TRUE";
     public static final String QUESTION_CHECK_BOX_TYPE = "M";
     public static final String QUESTION_RADIO_BUTTON_TYPE = "S";
     public static final String QUESTION_EDIT_TEXT_TYPE = "T";
